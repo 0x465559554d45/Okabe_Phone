573dd95e94cfbf1cec00ee3cc64a2436a5dbd1fa
FuyukoAyumu <127882134+0x465559554d45@users.noreply.github.com>
2026-05-23 23:55:15 +0200

    Add POST requests

--- src/System/HTTP.cpp
+++ src/System/HTTP.cpp
@@ -1,7 +1,8 @@
 #include "HTTP.h"
 
 enum HTTPMethod {
-    GET = 0
+    GET  = 0,
+    POST = 1,
 };
 
 struct HTTPHeader {
@@ -11,18 +12,23 @@ struct HTTPHeader {
     bool replace = true;
 };
 
-const char * sendRequest(NString URL, HTTPMethod method, HTTPHeader headers[] = nullptr) {
+const char * sendRequest(NString URL, HTTPMethod method, HTTPHeader headers[] = nullptr, char * POSTdata = nullptr) {
     HTTPClient http;
     http.begin(URL.c_str());
     http.setUserAgent("Nergon Mobile/0.2 SG01");
     if (headers != nullptr)
         for (int i = 0; i < sizeof(headers)/sizeof(headers[0]); i++)
             http.addHeader(headers[i].name, headers[i].value, headers[i].first, headers[i].replace);
+
+    if (POSTdata != nullptr) // Do a POST request regardless of the chosen method if data is present (needed for <form>s)
+        http.POST(POSTdata);
+
     switch (method) {
         case GET: {
             http.GET();
             return http.getString().c_str();;
             break;
         }
+        case POST : { break; } // POST request has already been handled above
     }
 }
